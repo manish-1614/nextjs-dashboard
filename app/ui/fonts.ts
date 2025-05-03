05975bfee4748a8f55450b13da8812d9c1c0d03c
Manish Kumar Prajapati <myindiaman@gmail.com>
2025-05-03 20:05:20 +0530

    Add Lusitana font import and export to fonts.ts. Unnecessary bugs were coming up bcoz of the absense of lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,6 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 import { Roboto } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
